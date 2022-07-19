d284a4aa162ca9e71dc7a2abb7ab1e6a7dbecfdf
davidebschke <Davidebschke8@gmail.com>
2022-07-19 10:47:22 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let breite= 100;
+let laenge=200;
+
+
+function produktberechnung(breite,laenge){
+
+let quadrat=breite*laenge;
+  console.log(quadrat);
+
+
+
+
+
+}
+
+function addieren(breite,laenge){
+
+  let addition=breite+laenge;
+  console.log(addition);
+
+
+
+
+
+}
+addieren(2,5);
+produktberechnung(2,5);
---
+++ ts/FirstTS.ts
@@ -0,0 +1,27 @@
+let breite: number= 100;
+let laenge:number =200;
+
+
+function produktberechnung(breite,laenge){
+
+  let quadrat=breite*laenge;
+  console.log(quadrat);
+
+
+
+
+
+}
+
+function addieren(breite,laenge){
+
+  let addition=breite+laenge;
+  console.log(addition);
+
+
+
+
+
+}
+addieren(2,5);
+produktberechnung(2,5);
